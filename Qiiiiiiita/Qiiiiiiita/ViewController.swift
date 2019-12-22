17632180f34e2bfd6ba1b803adbe74ee14c765dc
yo-i <yo.i198709@gmail.com>
2019-12-22 14:19:50 +0900

    [init]プロジェクト初期化

---
+++ Qiiiiiiita/Qiiiiiiita/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Qiiiiiiita
+//
+//  Created by 混沌の影 on 2019/12/22.
+//  Copyright © 2019 yo_i. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
