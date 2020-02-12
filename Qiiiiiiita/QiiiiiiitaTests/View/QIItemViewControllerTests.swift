3f60158d5a0a9679a3cd995139dbc84ff041bf4a
yang_yi <yang_yi@mti.co.jp>
2020-02-12 17:08:06 +0900

    [add]テストファイル作成

---
+++ Qiiiiiiita/QiiiiiiitaTests/View/QIItemViewControllerTests.swift
@@ -0,0 +1,33 @@
+//
+//  QIItemViewControllerTests.swift
+//  QiiiiiiitaTests
+//
+//  Created by 楊 逸帆 on 2020/02/07.
+//  Copyright © 2020 yo_i. All rights reserved.
+//
+
+import XCTest
+
+class QIItemViewControllerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
